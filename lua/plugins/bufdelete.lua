8cd42d86b905df184d583b1c7f917f870ab21183
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-10-11 20:13:32 +0200

    clean

--- lua/plugins/bufdelete.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "famiu/bufdelete.nvim",
-  init = function()
-    vim.keymap.set("n", "<leader>x", "<cmd> Bdelete<CR>", { noremap = true, silent = true, desc = "Delete Buffer" })
-  end,
-}
