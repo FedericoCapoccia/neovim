b091fd32fdbe03224be9fa23d8234b3cb237d2ee
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-10-11 20:12:29 +0200

    Who knows WTF?!

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "lua",
-        "nix",
-        "toml",
-        "vim",
-        "vimdoc",
-      },
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "cmake",
+                "cpp",
+                "lua",
+                "nix",
+                "rust",
+                "toml",
+                "vim",
+                "vimdoc",
+            },
 
-      auto_install = true,
+            auto_install = true,
 
-      indent = { enable = true },
-      autotag = { enable = true },
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-      },
-    })
-  end,
+            indent = { enable = true },
+            autotag = { enable = true },
+            highlight = {
+                enable = true,
+                use_languagetree = true,
+            },
+        })
+    end,
 }
