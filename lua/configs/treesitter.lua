6985d72396e9f973e7aa62983d9088ac7cbae52e
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-10-12 18:20:59 +0200

    folding

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -3,6 +3,13 @@ pcall(function()
     dofile(vim.g.base46_cache .. "treesitter")
 end)
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+
 return {
     ensure_installed = {
         "bash",
