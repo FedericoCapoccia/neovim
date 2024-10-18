1e498f4950c4c022359bb9bdbba3ed4f165ee7cd
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-10-18 14:21:39 +0200

    autopairs for cmp

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -6,10 +6,15 @@ return {
     {
         "windwp/nvim-autopairs",
         dependencies = "hrsh7th/nvim-cmp",
-        opts = {
-            fast_wrap = {},
-            disable_filetype = { "TelescopePrompt", "vim" },
-        },
+        config = function()
+            require("nvim-autopairs").setup {
+                fast_wrap = {},
+                disable_filetype = { "TelescopePrompt", "vim" },
+            }
+
+            local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+            require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
     },
 
     {
