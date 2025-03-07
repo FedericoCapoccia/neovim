c72b407ebaaa41758abcf9794c648a3692208475
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-03-07 02:11:45 +0100

    .

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -64,16 +64,16 @@ return {
     --     end,
     -- },
 
-    {
-        "toppair/peek.nvim",
-        event = { "VeryLazy" },
-        build = "deno task --quiet build:fast",
-        config = function()
-            require("peek").setup()
-            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-        end,
-    },
+    -- {
+    --     "toppair/peek.nvim",
+    --     event = { "VeryLazy" },
+    --     build = "deno task --quiet build:fast",
+    --     config = function()
+    --         require("peek").setup()
+    --         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    --         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    --     end,
+    -- },
 
     -- LSP
 
