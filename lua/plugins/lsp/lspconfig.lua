912cc72ef19a7976991afdde82031f110adf7985
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-03-16 21:44:23 +0100

    added message

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -31,6 +31,11 @@ M.setup = function()
     require("lint").linters_by_ft = {
         python = { "pylint" },
     }
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+            require("lint").try_lint()
+        end,
+    })
 
     require("crates").setup {
         lsp = {
