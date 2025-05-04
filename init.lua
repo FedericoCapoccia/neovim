9c7ace0c339b570c513013890a3e6ccb277911a1
FedericoCapoccia <federico.capoccia@windows.com>
2025-05-04 18:41:53 +0200

    tokyonight as default

--- init.lua
+++ init.lua
@@ -1,14 +1,13 @@
 require "configs"
 
--- vim.cmd "colorscheme onedark"
-vim.cmd "colorscheme rose-pine-moon"
+vim.cmd "colorscheme tokyonight"
 vim.api.nvim_set_hl(0, "@lsp.type.inlayHint", { fg = "#aaaaaa", bg = "NONE", italic = true })
 vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#888888", bg = "NONE", italic = true })
 
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
 -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
--- vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE" })
---
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE" })
+
 -- vim.opt.pumblend = 0
 -- vim.cmd [[
 --   highlight Pmenu guibg=none
---
+++ lua/plugins/colorschemes/gruvbox.lua
@@ -0,0 +1,10 @@
+local M = {
+    transparent_mode = true,
+}
+
+return {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = M,
+}
